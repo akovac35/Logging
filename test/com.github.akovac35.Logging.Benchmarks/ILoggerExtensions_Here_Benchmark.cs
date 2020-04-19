cfdf28845a5c54d6ac3543e85823bb86a37c0ca7
Aleksander Kovač <aleksander.kovac@gmail.com>
2020-04-19 20:17:54 +0200

    Added benchmarks.

---
+++ test/com.github.akovac35.Logging.Benchmarks/ILoggerExtensions_Here_Benchmark.cs
@@ -0,0 +1,36 @@
+﻿// License:
+// Apache License Version 2.0, January 2004
+
+// Authors:
+//   Aleksander Kovač
+
+using BenchmarkDotNet.Attributes;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace com.github.akovac35.Logging.Benchmarks
+{
+    [MinColumn, MaxColumn]
+    public class ILoggerExtensions_Here_Benchmark
+    {
+        private ILogger _logger = LoggerFactoryProvider.LoggerFactory.CreateLogger<ILoggerExtensions_Here_Benchmark>();
+
+        [Benchmark(Baseline = true)]
+        public DateTime EnteringExitingWithoutHere()
+        {
+            _logger.Entering();
+
+            _logger.Exiting();
+            return DateTime.Now;
+        }
+
+        [Benchmark]
+        public DateTime EnteringExitingWithHere()
+        {
+            _logger.Here(l => l.Entering());
+
+            _logger.Here(l => l.Exiting());
+            return DateTime.Now;
+        }
+    }
+}
---
+++ test/com.github.akovac35.Logging.Benchmarks/Program.cs
@@ -0,0 +1,15 @@
+﻿// License:
+// Apache License Version 2.0, January 2004
+
+// Authors:
+//   Aleksander Kovač
+
+using BenchmarkDotNet.Running;
+
+namespace com.github.akovac35.Logging.Benchmarks
+{
+    public class Program
+    {
+        public static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+    }
+}
